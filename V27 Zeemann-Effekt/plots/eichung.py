950b51ce424b2971d4511000f06ad86f73af7b69
Anneke <anneke.reinold@tu-dortmund.de>
2019-12-28 17:21:21 +0100

    Zwischenspeichern

--- V27 Zeemann-Effekt/plots/eichung.py
+++ V27 Zeemann-Effekt/plots/eichung.py
@@ -19,6 +19,17 @@ print('\nEichung:')
 print('a = ', params[0], r'\pm', err[0])
 print('b = ', params[1], r'\pm', err[1])
 
+print('\n _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_')
+a_fit=ufloat(183.320879, 7.8757597788)
+b_fit=ufloat(-36.0164822, 27.8450161)
+I_1=2.5
+I_2=3.8
+I_3=6.0
+B = a_fit*I_3+b_fit
+print(B)
+print('\n _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_')
+
+
 Ix = np.linspace(0, 6, 1000)
 
 plt.plot(I, Bauf, 'C2x', label='$B$ aufsteigend')
