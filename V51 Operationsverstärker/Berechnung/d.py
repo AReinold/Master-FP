f43256e1adcc0eef236911f299c862042ff605f5
Anneke <anneke.reinold@tu-dortmund.de>
2020-01-06 11:31:22 +0100

    Erster Teil der Korrektur

--- V51 Operationsverstärker/Berechnung/d.py
+++ V51 Operationsverstärker/Berechnung/d.py
@@ -15,7 +15,6 @@ plt.rcParams['text.latex.preamble'] = [r'\usepackage{siunitx}']
 
 f, U = np.genfromtxt('e.txt', unpack = True)
 
-
 #f_log=np.log(f)
 #U_log=np.log(U)
 plt.yscale('log')
@@ -26,15 +25,16 @@ def fx(x,m,b):
     return np.exp(np.log(x)*m+b)
 ParamsI, CovarianceI = curve_fit(fx, f[1:6], U[1:6])
 #ParamsI, CovarianceI = curve_fit(fx, f_log, U_log)
-i1 = np.linspace(10, 2500, 1000)
+i1 = np.linspace(50, 2500, 1000)
 ErrorsI = np.sqrt(np.diag(CovarianceI))
 m = ufloat(ParamsI[0], ErrorsI[0])
 b = ufloat(ParamsI[1], ErrorsI[1])
 print(m,b)
 plt.plot(i1, fx(i1, *ParamsI), 'b-', label='Lineare Regression')
-plt.ylabel(r'Ausgangsspannung $[\si{\mV}]$',fontsize = 12)
-plt.xlabel(r'Frequenz  $[\si{\Hz}]$',fontsize = 12)
+plt.ylabel(r"$U_\text{A} \:/\:$mV")
+plt.xlabel(r"$\nu \:/\:$Hz")
 plt.legend()
+plt.savefig('diffe.pdf')
 #plt.xscale('log')
 #plt.yscale('log')
 plt.show()
